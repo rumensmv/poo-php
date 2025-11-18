8f705dc49295163557d4a4724fe7ab77abd1e548
Rumen Simov <Rumen_Simov@etu.u-bourgogne.fr>
2025-11-18 08:22:34 +0100

    Créez vos propres classes

--- index.php
+++ index.php
@@ -5,6 +5,28 @@ const RESULT_LOSER = -1;
 const RESULT_DRAW = 0;
 const RESULT_POSSIBILITIES = [RESULT_WINNER, RESULT_LOSER, RESULT_DRAW];
 
+class Player
+{
+    public int $level;
+}
+
+class Encounter
+{
+    function probabilityAgainst(int $levelPlayerOne, int $againstLevelPlayerTwo)
+    {
+        return 1/(1+(10 ** (($againstLevelPlayerTwo - $levelPlayerOne)/400)));
+    }
+
+    function setNewLevel(int &$levelPlayerOne, int $againstLevelPlayerTwo, int $playerOneResult)
+    {
+        if (!in_array($playerOneResult, RESULT_POSSIBILITIES)) {
+            trigger_error(sprintf('Invalid result. Expected %s',implode(' or ', RESULT_POSSIBILITIES)));
+        }
+        $levelPlayerOne += (int) (32 * ($playerOneResult - probabilityAgainst($levelPlayerOne, $againstLevelPlayerTwo)));
+    }
+    
+}
+
 function probabilityAgainst(int $levelPlayerOne, int $againstLevelPlayerTwo)
 {
     return 1/(1+(10 ** (($againstLevelPlayerTwo - $levelPlayerOne)/400)));
